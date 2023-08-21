0f19d3bbee5213c656c0162f393c37d82438e1df
Riyaz Siddiqui <127213672+siddiq0611@users.noreply.github.com>
2023-08-21 23:55:50 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav_List')
+
+function toggleButton(){
+	navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+document.querySelectorAll('a[href^="#about-me"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#certi"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
